6b2d6fadc2a2100be8053db5772919c053e2e77e
chelcy <chelcy.nico@gmail.com>
2016-04-05 17:42:38 +0900

    initial commit

---
+++ Fuwa/src/main/java/net/mchel/plugin/fuwa/Fuwa.java
@@ -0,0 +1,17 @@
+package net.mchel.plugin.fuwa;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author Chelcy
+ *         2016/04/05
+ */
+public class Fuwa extends JavaPlugin{
+
+	@Override
+	public void onEnable() {
+		super.onEnable();
+	}
+
+
+}
---
+++ Fuwa/src/main/java/net/mchel/plugin/fuwa/listener/PlayerListener.java
@@ -0,0 +1,10 @@
+package net.mchel.plugin.fuwa.listener;
+
+import org.bukkit.event.Listener;
+
+/**
+ * @author Chelcy
+ *         2016/04/05
+ */
+public class PlayerListener implements Listener{
+}
